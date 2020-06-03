c55171e97ba1fcd7e1a62510e73d1d8139c24bd1
mellownightpirate <a@hasan.co>
2020-06-03 15:01:23 +0100

    Fill out Player and Room classes in player.py and room.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+
+    def __str__(self):
+        return (f'Welcome {self.name}! You have taken the red pill and have woken up at {self.room}. Shall we see how far the rabbit hole goes?')
